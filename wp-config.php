1a30e7fd3078a3fcbb893bb4db93600f4edaf884
wahid-ahmed <wahidahmed1992@gmail.com>
2022-11-08 09:29:44 +0600

    wp-config change

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wahid' );
+define( 'DB_NAME', 'wp8' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'wp_user8' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'password@12#' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
